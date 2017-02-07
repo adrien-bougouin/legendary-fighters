b1c85abf9d6fa31f21397c25be2ccfd5a36d587a
Adrien Bougouin <adrien.bougouin@gmail.com>
2017-02-07 22:19:18 +0900

    sdl exit

--- src/core/graphics.cpp
+++ src/core/graphics.cpp
@@ -9,6 +9,7 @@ Graphics::Graphics(): window_(NULL), renderer_(NULL) {
 Graphics::~Graphics() {
   stop();
   SDL_VideoQuit();
+  atexit(SDL_Quit);
 }
 
 void Graphics::start() {
@@ -17,15 +18,18 @@ void Graphics::start() {
 
   if (window_ == NULL) {
     // TODO error
+    stop();
   }
 
   renderer_ = SDL_CreateRenderer(window_, -1, 0);
 
   if (renderer_ == NULL) {
     // TODO error
+    stop();
   }
 }
 
+// TODO use a variadic cleanup function for SDL objects???
 void Graphics::stop() {
   if (renderer_ != NULL) {
     SDL_DestroyRenderer(renderer_);
