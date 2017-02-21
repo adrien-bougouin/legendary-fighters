abdae8a34ce59878fb66c561f31ee97807f7308e
Adrien Bougouin <adrien.bougouin@gmail.com>
2017-02-21 22:54:26 +0900

    add todos

--- src/components/inputs_component.cpp
+++ src/components/inputs_component.cpp
@@ -7,15 +7,19 @@ void InputsComponent::update(GameEntity &game_entity, Input input) {
   switch (input.type) {
     case InputType::LEFT:
       if (input.method == InputMethod::PUSH) {
+        // TODO use physics component
         velocity.set_x(velocity.x() - 1.0); // TODO use acceleration
       } else if (input.method == InputMethod::RELEASE) {
+        // TODO use physics component
         velocity.set_x(0.0);
       }
       break;
     case InputType::RIGHT:
       if (input.method == InputMethod::PUSH) {
+        // TODO use physics component
         velocity.set_x(velocity.x() + 1.0); // TODO use acceleration
       } else if (input.method == InputMethod::RELEASE) {
+        // TODO use physics component
         velocity.set_x(0.0);
       }
       break;
