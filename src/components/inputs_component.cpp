4aa1967288736837b8a3ac12d0cea7a30de7a64f
Adrien Bougouin <adrien.bougouin@gmail.com>
2017-02-24 00:11:48 +0900

    messing with physics

--- src/components/inputs_component.cpp
+++ src/components/inputs_component.cpp
@@ -2,34 +2,31 @@
 #include "../models/game_entity.hpp"
 
 void InputsComponent::update(GameEntity &game_entity, Input &input) {
-  Vector2D &velocity = game_entity.velocity();
-
   switch (input.type) {
     case InputType::LEFT:
       if (input.method == InputMethod::PUSH) {
-        // TODO use physics component
-        velocity.set_x(velocity.x() - 1.0); // TODO use acceleration
+        game_entity.physics_component().move_left(game_entity);
       } else if (input.method == InputMethod::RELEASE) {
-        // TODO use physics component
-        velocity.set_x(0.0);
+        game_entity.physics_component().stop_horizontal_motion(game_entity);
       }
       break;
     case InputType::RIGHT:
       if (input.method == InputMethod::PUSH) {
-        // TODO use physics component
-        velocity.set_x(velocity.x() + 1.0); // TODO use acceleration
+        game_entity.physics_component().move_right(game_entity);
       } else if (input.method == InputMethod::RELEASE) {
-        // TODO use physics component
-        velocity.set_x(0.0);
+        game_entity.physics_component().stop_horizontal_motion(game_entity);
       }
       break;
     case InputType::UP:
       if (input.method == InputMethod::PUSH) {
-        // TODO use physics component
-        velocity.set_y(velocity.y() + 1.0); // TODO use acceleration
+        // if not already jumping
+        if (game_entity.position().y() == 0.0) { // TODO remove this check
+          game_entity.physics_component().jump(game_entity);
+        } else {
+          game_entity.physics_component().stop_vertical_motion(game_entity);
+        }
       } else if (input.method == InputMethod::RELEASE) {
-        // TODO use physics component
-        velocity.set_y(0.0);
+        game_entity.physics_component().stop_vertical_motion(game_entity);
       }
       break;
     default:
--- src/components/physics_component.cpp
+++ src/components/physics_component.cpp
@@ -2,10 +2,63 @@
 #include "../models/game_entity.hpp"
 #include "../models/vector_2d.hpp"
 
+PhysicsComponent::PhysicsComponent(): max_x_velocity_(5.0), max_y_velocity_(40.0), x_acceleration_(5.0), y_acceleration_(40.0) {
+}
+
 void PhysicsComponent::update(GameEntity &game_entity) {
   Vector2D &position = game_entity.position();
+
+  position += game_entity.velocity();
+
+  // dumb gravity
+  position.set_y(position.y() - 8.0);
+  if (position.y() < 0.0) {
+    position.set_y(0.0);
+  }
+}
+
+void PhysicsComponent::stop_horizontal_motion(GameEntity &game_entity) {
   Vector2D &velocity = game_entity.velocity();
 
-  position += velocity;
+  velocity.set_x(0.0);
+}
+
+void PhysicsComponent::stop_vertical_motion(GameEntity &game_entity) {
+  Vector2D &velocity = game_entity.velocity();
+
+  velocity.set_y(0.0);
+}
+
+void PhysicsComponent::move_left(GameEntity &game_entity) {
+  Vector2D &velocity = game_entity.velocity();
+  double new_velocity = velocity.x() - x_acceleration_;
+
+  if (new_velocity < -max_x_velocity_) {
+    new_velocity = -max_x_velocity_;
+  }
+
+  velocity.set_x(new_velocity);
+}
+
+void PhysicsComponent::move_right(GameEntity &game_entity) {
+  Vector2D &velocity = game_entity.velocity();
+  double new_velocity = velocity.x() + x_acceleration_;
+
+  if (new_velocity > max_x_velocity_) {
+    new_velocity = max_x_velocity_;
+  }
+
+  velocity.set_x(new_velocity);
+}
+
+void PhysicsComponent::jump(GameEntity &game_entity) {
+  Vector2D &velocity = game_entity.velocity();
+  double new_velocity = velocity.y() + y_acceleration_;
+
+  if (new_velocity > max_y_velocity_) {
+    new_velocity = max_y_velocity_;
+  }
+
+  velocity.set_y(new_velocity);
 }
 
