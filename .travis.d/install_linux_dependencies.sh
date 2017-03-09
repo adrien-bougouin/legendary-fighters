077276fcf97209ebe0ce61b73c216d533f3df8d5
Adrien Bougouin <adrien.bougouin@gmail.com>
2017-03-09 23:08:19 +0900

    fix travis linux test by using repository containing libsdl2-image-dev

---
+++ .travis.d/install_linux_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
+sudo apt-get update
+sudo apt-get install libsdl2-dev libsdl2-image-dev
+sudo apt-get install libcppunit-dev
+
---
+++ .travis.d/install_osx_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+brew update
+brew install cppunit
+brew install sdl2 sdl2_image
+
